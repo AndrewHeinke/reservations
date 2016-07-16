26b96e4719dd60ff07f06ee9f78c393945f60e65
Andrew <andrew.j.heinke@gmail.com>
2016-07-16 11:28:30 -0500

    added public folder

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, 'home.html'));
